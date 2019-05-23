552484a9dfde03914f74734563e37b0799a9f593
Lukie Kang <lukiek+f@gmail.com>
2019-05-23 06:16:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,6 +12,7 @@ def batch_badge_creator(names)
   array
 end
 
+<<<<<<< HEAD
 def assignment(person,index)
    "Hello, #{person}! You'll be assigned to room #{index + 1}!"
 end
@@ -20,15 +21,28 @@ def assign_rooms(speakers)
   assignments = []
   speakers.each_with_index do | speaker, index |
     assignments << assignment(speaker,index)
+=======
+def assign_rooms(speakers)
+  assignments = []
+  speakers.each_with_index do | speaker, index |
+    assignments << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+>>>>>>> 2b9db1aeda5815f40cafaf37bbb6082d80af8feb
   end
   assignments
 end
 
 def printer(attendees)
+<<<<<<< HEAD
   attendees.each_with_index do |person, index|
   puts badge_maker(person)
   puts assignment(person,index)
  end
+=======
+  attendees.each do |person|
+  puts badge_maker(person)
+  puts assign_rooms([person])
+end
+>>>>>>> 2b9db1aeda5815f40cafaf37bbb6082d80af8feb
 
 
 end
